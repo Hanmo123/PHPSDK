1135bb6c5239d4e2824ae764816d89017bbd7f4a
Hanmo <1374996108@qq.com>
2021-10-05 23:22:13 +0800

    更改目录

---
+++ src/CCS/getInstanceList.php
@@ -0,0 +1,19 @@
+<?php
+
+use AtomCloudSDK\Credential;
+use AtomCloudSDK\Client;
+
+require_once __DIR__ . '/../../Credential.php';
+require_once __DIR__ . '/../../Client.php';
+require_once __DIR__ . '/../../Service/CCS.php';
+
+$cred = new Credential(1, '67ee09def916d863', '4c2141ae015b68a562a0c3734794432d50509a83a9307a118800bcd129b902a42acc157fcbcc0148aad270bf469c0a2b963060f95c9824b5cc65c9380ed38ccff3dc73243984daf6d0ff6d366a1ab5734b072fe76449d83d510ce23b36698cc9f3432a56ddf7288f79a5269082125ebc1c59afa4bfef1029f24a46c3b37cfa50');
+
+$client = new Client;
+
+$service = new AtomCloudSDK\Service\CCS();
+$return = $service->setCredential($cred)
+    ->setClient($client)
+    ->getInstanceList();
+
+print_r($return);
